8704625812767f6dd6183fcbd5304033d40eafe0
LepeshkiIvan <begaevnikita@gmail.com>
2024-05-30 10:23:31 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindie1backend.nomoredomainswork.ru/api";
+export const BASE_URL = "https://ravager.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
